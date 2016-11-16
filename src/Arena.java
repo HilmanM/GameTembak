37d10dffbc9b76dfcc275a2da31778859ec00ad3
Fengkie Junis <fengkiejunis@yahoo.co.id>
2016-11-17 01:19:17 +0700

    Masih incomplete

--- src/Arena.java
+++ src/Arena.java
@@ -1,2 +1,21 @@
 public class Arena {
+  private int nomorArena;
+  private int waktu;
+
+  public int ambilNomorArena(){
+    return this.nomorArena;
+  }
+
+  public boolean kurangiWaktu(){
+    this.waktu--;
+    if(this.waktu==0){
+      return false;
+    }
+    
+    return true;
+  }
+
+  public void naikArena(){
+    this.nomorArena++;
+  }
 }
